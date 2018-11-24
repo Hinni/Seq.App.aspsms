eff56756ec7c593862978e24c715d4c368641212
hinni <michael.hinni@pm.me>
2018-11-24 01:21:57 +0100

    Fix CreatePackage path to NuGet.CommandLine after update

--- CreatePackage.ps1
+++ CreatePackage.ps1
@@ -3,4 +3,4 @@
 $directory = Split-Path $MyInvocation.MyCommand.Path
 
 # Create NuGet Package
-& $directory\packages\NuGet.CommandLine.4.5.1\tools\NuGet.exe pack $directory\Seq.App.aspsms\Seq.App.aspsms.csproj -OutputDirectory $directory -Prop Configuration=Release
+& $directory\packages\NuGet.CommandLine.4.7.1\tools\NuGet.exe pack $directory\Seq.App.aspsms\Seq.App.aspsms.csproj -OutputDirectory $directory -Prop Configuration=Release
