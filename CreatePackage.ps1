813415e631f4460896b74ce57858877fb5aa08d8
Hinni <michael@hinni.solutions>
2019-02-01 20:56:25 +0100

    Clean up old projects and travis

--- CreatePackage.ps1
+++
@@ -1,5 +0,0 @@
-# Arrange
-$directory = Split-Path $MyInvocation.MyCommand.Path
-
-# Create NuGet Package
-& $directory\packages\NuGet.CommandLine.4.9.2\tools\NuGet.exe pack $directory\Seq.App.aspsms\Seq.App.aspsms.csproj -OutputDirectory $directory -Prop Configuration=Release
--- Seq.App.aspsms/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Seq.App.aspsms")]
-[assembly: AssemblyDescription("Allows you to notify a mobile phone by text message via aspsms.com")]
